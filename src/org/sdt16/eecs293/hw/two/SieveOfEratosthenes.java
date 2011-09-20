c5ab6f8011fb84418db34d79796c83c33ff92b45
Schuyler Thompson <skooz@skooz.net>
2011-09-20 18:08:37 -0400

    Added javadoc to the methods.

--- src/org/sdt16/eecs293/hw/two/SieveOfEratosthenes.java
+++ src/org/sdt16/eecs293/hw/two/SieveOfEratosthenes.java
@@ -18,7 +18,24 @@ import java.util.LinkedList;
 public class SieveOfEratosthenes {
 
 	/**
-	 * @param args
+	 * First we set up our 2 lists. The numberList takes every number from 2 to maxNum.
+	 * The primeNumbers takes every number we know is prime.
+	 * 
+	 * First, we call getInput to handle parsing the argument from the command line.
+	 * 
+	 * Then, we fill in the numberList.
+	 * 
+	 * Then, while numberList isn't empty, we add the first number in the list to
+	 * primeNumbers, because we know that it must be prime. Then we call removeMultiples
+	 * to get rid of every multiple of the primeNumber still in the numberList, including
+	 * the number itself.
+	 * 
+	 * When numberList is empty, we know that every number has been determined to be
+	 * prime or composite.
+	 * 
+	 * Finally, we print out all of the numbers in primeNumbers.
+	 * 
+	 * @param args args[0] is the number to find primes up to.
 	 */
 	public static void main(String[] args) {
 		int maxNum = getInput(args);
@@ -31,8 +48,8 @@ public class SieveOfEratosthenes {
 		}
 		
 		while (!numberList.isEmpty()) {
-				primeNumbers.add(numberList.getFirst());
-				numberList = removeMultiples(numberList.getFirst(), numberList);
+			primeNumbers.add(numberList.getFirst());
+			numberList = removeMultiples(numberList.getFirst(), numberList);
 		}
 		
 		for (Integer primeNumber : primeNumbers) {
@@ -40,6 +57,15 @@ public class SieveOfEratosthenes {
 		}	
 	}
 
+	/**
+	 * This method checks to make sure the args from the command line are valid.
+	 * It checks that there is only 1 argument, that the arg is a number, and that the
+	 * number is at least 2.
+	 * 
+	 * If the argument is invalid, quit.
+	 * @param args The command line arguments
+	 * @return The parsed number from the command line.
+	 */
 	private static int getInput(String[] args) {
 		int maxNum = 0;
 		try {
@@ -57,6 +83,19 @@ public class SieveOfEratosthenes {
 		return maxNum;
 	}
 
+	/**
+	 * This method removes all of the multiples in the numberList of the prime
+	 * number passed in factor. It iterates though the list, and sees if the 
+	 * modulus of the number currently being tested to the factor is 0. If it
+	 * is, we know it is a multiple of the prime, so it can't be prime itself. 
+	 * It is therefore removed. 
+	 * 
+	 * This method has the advantage of having constant time removal, and iterating
+	 * over the list exactly once for every factor.
+	 * @param factor The prime number you want to remove multiples of.
+	 * @param numberList The numberList you want to remove the multiples from.
+	 * @return the new numberList with the multiples removed.
+	 */
 	private static LinkedList<Integer> removeMultiples(int factor, LinkedList<Integer> numberList) {
 		for (Iterator<Integer> it = numberList.iterator(); it.hasNext();) {
 			if (it.next() % factor == 0) {
